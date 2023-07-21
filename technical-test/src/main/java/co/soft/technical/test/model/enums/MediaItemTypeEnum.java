2bb1713361f85c81a45ef2dbc08c5468fd26f137
Julián Santiago Martinez Trullo <juliansmartinez@unicauca.edu.co>
2023-07-21 09:41:27 -0500

    add refactor src

--- technical-test/src/main/java/co/soft/technical/test/model/enums/MediaItemTypeEnum.java
+++
@@ -1,6 +0,0 @@
-package co.soft.technical.test.model.enums;
-
-public enum MediaItemTypeEnum {
-    MOVIE,
-    SERIES
-}
--- technical-test/src/main/java/co/soft/technical/test/security/model/enums/TypeRole.java
+++
@@ -1,5 +0,0 @@
-package co.soft.technical.test.security.model.enums;
-
-public enum TypeRole {
-    ROLE_ADMIN, ROLE_USER
-}
